ed04275b8637403dc6d8adc21f2cd6bf2dac6462
Tran Phuong Tuan <Tran Phuong Tuan@DESKTOP-3VUB9E5>
2018-02-02 10:05:19 +0700

    Add project files.

---
+++ phuongtrinhbac2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phuongtrinhbac2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+
+            float del = (b * b) - (4 * a * c);
+            if (del < 0)
+                Console.WriteLine("Phuong trinh vo nghiem");
+            else if (del == 0)
+            {
+                float x = -b / (2 * a);
+            }
+            else
+            {
+                //.....
+            }
+        }
+    }
+}
